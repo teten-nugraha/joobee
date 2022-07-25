d0807ca644326e013c7b0df37ee2adddc8e60c46
Teten Nugraha <teten.nugraha18@gmail.com>
2022-07-25 15:13:34 +0700

    create and update job

--- joobee/settings.py
+++ joobee/settings.py
@@ -139,3 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 GDAL_LIBRARY_PATH = '/opt/homebrew/opt/gdal/lib/libgdal.dylib'
 GEOS_LIBRARY_PATH = '/opt/homebrew/opt/geos/lib/libgeos_c.dylib'
+
+APPEND_SLASH=False
