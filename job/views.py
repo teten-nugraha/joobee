f49542945dc417f4cc2d1b91620eb2e32a34be81
Teten Nugraha <teten.nugraha@gojek.com>
2022-07-25 14:23:03 +0700

    getAllJobs endpoint

--- job/views.py
+++ job/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import JobSerializer
+from .models import Job
+
+@api_view(['GET'])
+def getAllJobs(request):
+
+    jobs = Job.objects.all()
+
+    serializer = JobSerializer(jobs, many=True)
+    return Response(serializer.data)
