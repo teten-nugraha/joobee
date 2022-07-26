02a44fcf76a716b02cabaf92ad49912cad1b7b3f
Teten Nugraha <teten.nugraha18@gmail.com>
2022-07-26 17:06:04 +0700

    apply search

--- job/filters.py
+++ job/filters.py
@@ -3,9 +3,11 @@ from .models import Job
 
 class JobFilter(filters.FilterSet):
 
+    keyword    = filters.CharFilter(field_name='title', lookup_expr='icontains')
+    location   = filters.CharFilter(field_name='address', lookup_expr='icontains')
     min_salary = filters.NumberFilter(field_name="salary" or 0, lookup_expr='gte')
     max_salary = filters.NumberFilter(field_name="salary" or 1000000, lookup_expr='lte')
 
     class Meta:
         model = Job
-        fields = ('education','jobType','experience','min_salary','max_salary')
+        fields = ('keyword','location','education','jobType','experience','min_salary','max_salary')
