23acbf7980ca0f94edbcb34a6d3078eb22a14cd8
Teten Nugraha <teten.nugraha18@gmail.com>
2022-07-27 10:02:52 +0700

    apply serializer and register api

--- joobee/settings.py
+++ joobee/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 import dotenv
 
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     
     'job.apps.JobConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -114,6 +116,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken'),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
--- joobee/urls.py
+++ joobee/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('job.urls'))
+    path('api/', include('job.urls')),
+    path('api/', include('account.urls'))
 ]
