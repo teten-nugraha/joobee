301e60b549ca6060687af84f6e14a70705948c3c
Teten Nugraha <teten.nugraha18@gmail.com>
2022-07-29 14:13:11 +0700

    me jobs

--- job/urls.py
+++ job/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('jobs/<str:pk>/apply', views.applyToJob, name='apply_to_job'),
     path('me/jobs/applied', views.getCurrentUserAppliedJobs, name='current_user_applied_job'),
     path('jobs/<str:pk>/check', views.isApplied, name='is_applied_to_jo'),
+    path('me/jobs', views.getCurrentUserJobs, name='current_user_jobs'),
 
 ]
